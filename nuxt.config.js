8566da575af6f9eba841ac239d20852b26d3c9bb
Kiran <jasvanee.kiran@gmail.com>
2020-10-28 23:18:36 +0530

    fixed repo name

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   router: {
-    base: '/nuxtpracticeone/'
+    base: '/nuxtjs_practice_first/'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
